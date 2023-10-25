abe39a95c0ee167d8deb57400acaf886e3d0cbf7
Juan Melguizo <122971281+juanmelmos@users.noreply.github.com>
2023-10-25 12:17:23 +0200

    Cambiamos de m1.small a t2.micro

--- ej3_ec2.sh
+++ ej3_ec2.sh
@@ -12,7 +12,7 @@ aws ec2 authorize-security-group-ingress \
 aws ec2 run-instances \
 --image-id ami-050406429a71aaa64 \
 --count 1  \
---instance-type m1.small \
+--instance-type t2.micro \
 --key-name vockey \
 --region us-east-1 \
 --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=EC2JUAN3}]' \
